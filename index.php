f8dc4793cbf37dfc9370eb8e61710645b2e4b23d
DavidMayorgaS08 <davidmayorga@colegioguanenta.edu.co>
2022-08-17 18:24:19 -0500

    Avances registro

--- index.php
+++ index.php
@@ -26,34 +26,3 @@ if (isset($_POST['submit'])) {
 }
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-	<link rel="stylesheet" type="text/css" href="stylefor.css">
-
-	<title>Registro</title>
-</head>
-<body>
-	<div class="container">
-		<form action="" method="POST" class="login-email">
-			<p class="login-text" style="font-size: 2rem; font-weight: 800;">INICIO</p>
-			<div class="input-group">
-				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
-			</div>
-			<div class="input-group">
-				<input type="password" placeholder="identificación" name="password" value="<?php echo $_POST['password']; ?>" required>
-			</div>
-			<div class="input-group">
-				<button name="submit" class="btn">Entrar</button>
-			</div>
-			<center><p class="login-register-text">¿No tienes una cuenta? <br/> ><a href="register.php">Registrarte Ahora</a>.</p></center>
-		</form>
-	</div>
-</body>
-</html>
